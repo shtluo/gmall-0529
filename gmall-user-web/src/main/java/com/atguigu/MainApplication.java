e55b3bebc015b8509a9ec5e163e3db7d2d0d41c2
罗世涛 <2021712351@qq.com>
2018-10-09 13:51:15 +0800

    4444

---
+++ gmall-user-web/src/main/java/com/atguigu/MainApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableDubbo
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
--- gmall-user-web/src/main/java/com/atguigu/gmall/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/UserController.java
@@ -1,7 +1,23 @@
 package com.atguigu.gmall;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.movie.MovieService;
+import com.atguigu.gmall.user.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+    @Reference
+    MovieService movieService;
+
+
+    @ResponseBody
+    @RequestMapping("/movie")
+    public Movie buyTicket(String userId, String mid){
+        Movie movie = movieService.getMovie(mid);
+
+        return movie;
+    }
 }
